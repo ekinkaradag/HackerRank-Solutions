97282f46b37c4879cb684d5c101f95e75aabd53d
Ekin Karadag <6624768+ekinkaradag@users.noreply.github.com>
2019-03-09 00:35:16 +0300

    Added a new solution
    Added a new Java solution (Java String Tokens).

---
+++ Java/Strings/Java String Tokens/java-string-tokens.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        
+        s = s.trim();
+        if (s.length() == 0) {
+            System.out.println(0);
+            return;
+        }
+        String [] words = s.split("[ !,?._'@]+");
+
+        System.out.println(words.length);
+        for (String word : words) {
+            System.out.println(word);
+        }
+
+        scan.close();
+    }
+}
+
